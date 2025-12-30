c9858a9ad944cc68feb6eb53ab61b2e4f094687e
Cristian <cpulidodev3@gmail.com>
2025-12-30 16:08:24 -0600

    feat: redesign users table with UUIDs and timestamps

--- src/server.js
+++ src/server.js
@@ -1,16 +1,5 @@
 const app = require('./app');
 const env = require('./config/env');
-const pool = require('./config/db');
-
-(async () => {
-    try {
-        const [rows] = await pool.query('SELECT NOW()');
-        console.log('Mysql connected: ', rows);
-    } catch (error) {
-        console.error('Mysql connection error:', error.message);
-        process.exit(1);
-    }
-})();
 
 app.listen(env.port, () => {
     console.log(`Server running on port ${env.port}`);
