37456ad01dbc67023238695687a7329ad7277575
Cristian <cpulidodev3@gmail.com>
2026-01-05 10:58:29 -0600

    feat: add task model with user-based queries

---
+++ src/models/task.model.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4} from 'uuid';
+import pool from '../config/db.js';
+
+export async function createTask({ userId, title}) {
+    const id = uuidv4();
+
+    const query = `INSERT INTO tasks (id, user_id, title)
+    VALUES (?,?,?)`;
+
+    await pool.execute(query, [id, userId, title]);
+
+    return {
+        id,
+        title,
+        completed: false,
+    };
+}
+
+export async function getTasksByUser(userId) {
+
+    const query = `
+    SELECT id, title, completed, created_at
+    FROM tasks 
+    WHERE user_id = ?
+    ORDER BY CREATED_AT DESC
+    `
+    const [rows] = await pool.execute(query, [userId])
+}
